2937d365dacb668554aa16800cca2246d8ed092b
Max Pronko <max.pronko@gmail.com>
2019-08-16 21:30:03 +0300

    Added CacheButton service

--- Plugin/CachePlugin.php
+++ Plugin/CachePlugin.php
@@ -5,12 +5,27 @@ namespace Pronko\SelectiveCache\Plugin;
 
 use Magento\Backend\Block\Cache;
 use Magento\Framework\View\LayoutInterface;
+use Pronko\SelectiveCache\Service\CacheButton;
 
 /**
  * Class CachePlugin
  */
 class CachePlugin
 {
+    /**
+     * @var CacheButton
+     */
+    private $cacheButton;
+
+    /**
+     * CachePlugin constructor.
+     * @param CacheButton $cacheButton
+     */
+    public function __construct(CacheButton $cacheButton)
+    {
+        $this->cacheButton = $cacheButton;
+    }
+
     /**
      * @param Cache $subject
      * @param LayoutInterface $layout
@@ -19,21 +34,6 @@ class CachePlugin
         Cache $subject,
         LayoutInterface $layout
     ) {
-        $subject->addButton(
-            'flush_invalidated_only',
-            [
-                'label' => __('Flush Invalidated Only'),
-                'onclick' => 'setLocation(\'' . $this->getFlushInvalidatedOnlyUlr() . '\')',
-                'class' => 'primary flush-cache-magento'
-            ]
-        );
-    }
-
-    /**
-     * @return string
-     */
-    private function getFlushInvalidatedOnlyUlr()
-    {
-        return '';
+        $this->cacheButton->execute($subject);
     }
 }
---
+++ Service/CacheButton.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pronko\SelectiveCache\Service;
+
+use Magento\Backend\Block\Cache;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Class CacheButton
+ */
+class CacheButton
+{
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+
+    /**
+     * CacheButton constructor.
+     * @param UrlInterface $url
+     */
+    public function __construct(UrlInterface $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @param Cache $cache
+     */
+    public function execute(Cache $cache)
+    {
+        $cache->addButton(
+            'flush_invalidated_only',
+            [
+                'label' => __('Flush Invalidated Only'),
+                'onclick' => 'setLocation(\'' . $this->getFlushInvalidatedOnlyUlr() . '\')',
+                'class' => 'primary flush-cache-magento'
+            ]
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function getFlushInvalidatedOnlyUlr()
+    {
+        return $this->url->getUrl('adminhtml/*/flushAll');
+    }
+}
