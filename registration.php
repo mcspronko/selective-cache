17f58eea357802b459c99787ec8311253e37713f
Max Pronko <max.pronko@gmail.com>
2019-08-16 20:23:52 +0300

    Added registration.php and module.xml

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Pronko Consulting (https://www.pronkoconsulting.com)
+ * See LICENSE for the license details.
+ */
+declare(strict_types=1);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Pronko_SelectiveCache',
+    __DIR__
+);
