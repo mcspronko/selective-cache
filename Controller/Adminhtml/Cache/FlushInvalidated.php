e244784dc8b7313e474f2f034f55f2ec9e67f771
Max Pronko <max.pronko@gmail.com>
2019-08-23 20:02:53 +0300

    Added invalidation logic into the controller

--- Controller/Adminhtml/Cache/FlushInvalidated.php
+++ Controller/Adminhtml/Cache/FlushInvalidated.php
@@ -2,18 +2,35 @@
 
 namespace Pronko\SelectiveCache\Controller\Adminhtml\Cache;
 
-use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Backend\Controller\Adminhtml\Cache;
+use Magento\Framework\DataObject;
+use Magento\Framework\Controller\ResultInterface;
 
 /**
  * Class FlushInvalidated
  */
 class FlushInvalidated extends Cache implements HttpGetActionInterface
 {
+    /**
+     * @return ResultInterface
+     */
     public function execute()
     {
-        $this->messageManager->addSuccessMessage(__("The Flush Invalidated Only logic has to be implemented here."));
+        $flag = false;
+        /** @var DataObject $invalidatedType */
+        foreach ($this->_cacheTypeList->getInvalidated() as $invalidatedType) {
+            $this->_cacheTypeList->cleanType($invalidatedType->getData('id'));
+            $flag = true;
+        }
+
+        if ($flag) {
+            $this->messageManager->addSuccessMessage(__("Invalidated caches have been successfully cleaned."));
+        } else {
+            $this->messageManager->addNoticeMessage(__("There are no invalidated cache types to be cleaned."));
+        }
+
         $resultRedirect = $this->resultRedirectFactory->create();
-        return $resultRedirect->setPath('adminhtml/*');
+        return $resultRedirect->setPath('adminhtml/cache');
     }
 }
