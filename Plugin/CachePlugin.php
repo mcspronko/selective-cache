627b288b646a6dba749ae91f6a94af8a355fc05c
Max Pronko <max.pronko@gmail.com>
2019-08-24 09:51:00 +0300

    Added MIT License

--- Plugin/CachePlugin.php
+++ Plugin/CachePlugin.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Copyright © Pronko Consulting (https://www.pronkoconsulting.com)
+ * See LICENSE for the license details.
+ */
 declare(strict_types=1);
 
 namespace Pronko\SelectiveCache\Plugin;
