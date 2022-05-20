95009727a586ea185504a31aad73e49666ac29ee
nordcomputer <13378398+nordcomputer@users.noreply.github.com>
2022-05-20 12:29:58 +0200

    run cache cleaning by cronjob

---
+++ Cron/FlushInvalidatedCacheTypes.php
@@ -0,0 +1,67 @@
+<?php
+namespace Pronko\SelectiveCache\Cron;
+
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/** Class FlushInvalidatedCacheTypes flushes invalidated cache types by cronjob */
+
+class FlushInvalidatedCacheTypes
+{
+
+    /**
+     * @var TypeListInterface
+     */
+    protected $_cacheTypeList;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * FlushInvalidatedCache constructor.
+     *
+     * @param TypeListInterfaceAlias $cacheTypeList
+     * @param ScopeConfigInterface $scopeConfig
+     */
+
+    public function __construct(
+        TypeListInterface $cacheTypeList,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_cacheTypeList = $cacheTypeList;
+        $this->_scopeConfig = $scopeConfig;
+    }
+    
+    /**
+     * Method execute flushes invalidated Cache types
+     *
+     * @return void
+     */
+
+    public function execute()
+    {
+        $cronenabled = $this->getConfig('selectivecache/general/enabled');
+        if ($cronenabled==true) {
+            foreach ($this->_cacheTypeList->getInvalidated() as $invalidatedType) {
+                $this->_cacheTypeList->cleanType($invalidatedType->getData('id'));
+            }
+        }
+    }
+
+    /**
+     * Get Configuration values
+     *
+     * @param string $config_path
+     * @return string|null
+     */
+
+    public function getConfig(string $config_path): ?string
+    {
+        return $this->_scopeConfig->getValue(
+            $config_path,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+}
