73a8d91f4addd4f27c1eeeb6577fd9deca5748d5
Mario Maresch <13378398+nordcomputer@users.noreply.github.com>
2022-06-08 16:07:53 +0200

    - removed getConfig method for isFlag method - updated variable names and removed underscores from methods

--- Cron/FlushInvalidatedCacheTypes.php
+++ Cron/FlushInvalidatedCacheTypes.php
@@ -18,34 +18,34 @@ class FlushInvalidatedCacheTypes
     /**
      * @var TypeListInterface
      */
-    private $_cacheTypeList;
+    private $cacheTypeList;
 
     /**
      * @var ScopeConfigInterface
      */
-    private $_scopeConfig;
+    private $scopeConfig;
 
     /**
      * @var CleanCacheLogger
      */
-    private $_logger;
+    private $logger;
 
     /**
      * FlushInvalidatedCache constructor.
      *
-     * @param TypeListInterfaceAlias $_cacheTypeList
-     * @param ScopeConfigInterface $_scopeConfig
-     * @param CleanCacheLogger $_logger
+     * @param TypeListInterfaceAlias $cacheTypeList
+     * @param ScopeConfigInterface $scopeConfig
+     * @param CleanCacheLogger $logger
      */
 
     public function __construct(
-        TypeListInterface $_cacheTypeList,
-        ScopeConfigInterface $_scopeConfig,
-        CleanCacheLogger $_logger
+        TypeListInterface $cacheTypeList,
+        ScopeConfigInterface $scopeConfig,
+        CleanCacheLogger $logger
     ) {
-        $this->_cacheTypeList = $_cacheTypeList;
-        $this->_scopeConfig = $_scopeConfig;
-        $this->_logger = $_logger;
+        $this->cacheTypeList = $cacheTypeList;
+        $this->scopeConfig = $scopeConfig;
+        $this->logger = $logger;
     }
 
     /**
@@ -56,31 +56,20 @@ class FlushInvalidatedCacheTypes
 
     public function execute()
     {
-        $cronenabled = $this->getConfig('selectivecache/cron/enabled');
+        $cronenabled = $this->scopeConfig->isSetFlag(
+            'selectivecache/cron/enabled',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
         if ($cronenabled==true) {
-            foreach ($this->_cacheTypeList->getInvalidated() as $invalidatedType) {
-                $this->_cacheTypeList->cleanType($invalidatedType->getData('id'));
+            foreach ($this->cacheTypeList->getInvalidated() as $invalidatedType) {
+                $this->cacheTypeList->cleanType($invalidatedType->getData('id'));
                 $cacheLabels[] = $invalidatedType->getData('cache_type');
             }
 
             if (!empty($cacheLabels)) {
-                $this->_logger->info(__("Cache types cleared automatically: %1", implode(',', $cacheLabels)));
+                $this->logger->info(__("Cache types cleared automatically: %1", implode(',', $cacheLabels)));
             }
         }
-    }
-
-    /**
-     * Get Configuration values
-     *
-     * @param string $config_path
-     * @return string|null
-     */
-
-    public function getConfig(string $config_path): ?string
-    {
-        return $this->_scopeConfig->getValue(
-            $config_path,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
     }
 }
